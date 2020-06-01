b28afc7b8f1324e5612e727e75a3f5e1fd1da4c2
meltingshine <meltingshine@gmail.com>
2020-06-01 19:26:04 +0900

    숙제 끝 !

--- app/src/main/java/com/example/myprofile_20200528/MainActivity.kt
+++ app/src/main/java/com/example/myprofile_20200528/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myprofile_20200528
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -19,6 +20,25 @@ class MainActivity : AppCompatActivity() {
             startActivityForResult(myIntent,REQ_FOR_NICK)
 
 
+
+        }
+
+        dialBtn.setOnClickListener {
+            val myNum = dialEdt.text.toString()
+            val myUri = Uri.parse("tel:${myNum}")
+            val myIntent = Intent(Intent.ACTION_DIAL, myUri)
+            startActivity(myIntent)
+        }
+
+        smsSendBtn.setOnClickListener {
+            val myNum = dialEdt.text.toString()
+            val myContent = smsContentEdt.text.toString()
+            val myUri = Uri.parse("smsto:${myNum}")
+            val myIntent = Intent(Intent.ACTION_SENDTO,myUri)
+            myIntent.putExtra("sms_body",myContent)
+            startActivity(myIntent)
+
+
         }
     }
 
@@ -28,7 +48,7 @@ class MainActivity : AppCompatActivity() {
 //            뭐때문에 갔니
             if(resultCode == Activity.RESULT_OK) {
 //                확인해서 돌아온게 맞니
-                val nickName = data?.getStringExtra("Nick")
+                val nickName = data?.getStringExtra("nick")
                 //get자료형Extra
                 myNicknameTxt.text = nickName
                 Toast.makeText(this,"닉네임변경완료",Toast.LENGTH_SHORT).show()
